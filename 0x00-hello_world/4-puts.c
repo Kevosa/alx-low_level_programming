79816161e58d1d22910af04a2d37e3283ac82caa
Kevosa <yegokelvin00@gmail.com>
2023-06-15 14:24:02 -0700

    task5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+include <stdio.h>
+
+/**
+ * min-entry point
+ *
+ * Return;Always 0(success)
+ * /
+
+int main(void);
+{
+puts"/"Programming is like building a multilingual puzzle";
+
+Return(0);
+}
+
