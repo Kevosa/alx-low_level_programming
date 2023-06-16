31221bed34aed28373abceb0750b7e2e92c2ef7f
Kevosa <yegokelvin00@gmail.com>
2023-06-16 04:38:18 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 
-/*
- * main - Entry point of the program
- *
- * This function prints the text followed by a new line
- *
- * Return:  Always 0 (success)
+/**
+ * main - Entry point of program
+ * Return: Always 0 (success)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,/n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
