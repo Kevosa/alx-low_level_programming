2a5e2eb94b77f06da8ebcc7d02ede88b40db8ec0
Kevosa <yegokelvin00@gmail.com>
2023-06-16 03:37:16 -0700

    task4.0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * main - to print given sentence
- * Return;Always 0(success)
+ * Return: Always 0(success)
  */
 
 int main(void)
@@ -10,6 +10,6 @@ int main(void)
 
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
 }
 
