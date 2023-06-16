4f0a404cd1e22b39e526c6e0976daec478af9fa9
Kevosa <yegokelvin00@gmail.com>
2023-06-16 04:52:05 -0700

    task6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Entry of program
+ * Return: Always 0(success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)"\n);
+	printf("Size of a int: %lu byte(s)"\n);
+	printf("Size of a long int: %lu byte(s)"\n);
+	printf("Size of a long long int: %lu byte(s)"\n);
+	printf("Size of a float: %lu byte(s)"\n);
+
+	return (0);
+}
