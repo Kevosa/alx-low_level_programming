2953f9700d54168ba52a821edcec81870c845210
Kevosa <yegokelvin00@gmail.com>
2023-06-17 06:12:24 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - print all single digits of base 10
+ *
+ * Return: alaways 0
+ */
+int main(void)
+{
+	int digit
+
+		for (digit = 0 ; digit < 10 ; digit++)
+		{
+			putchar(digit);
+		}
+	putchar('\n')
+		return (0);
+}
