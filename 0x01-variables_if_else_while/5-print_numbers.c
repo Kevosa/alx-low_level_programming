9a5106f87113e56f8f4d8877a0ac231f115c6dfb
Kevosa <yegokelvin00@gmail.com>
2023-06-17 05:52:04 -0700

    task4

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - print all single digit number to base 10
+ *
+ * Return:alaways0
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0 ; digit < 10 ; digit++)
+	{
+		printf(digit);
+	}
+	printf('\n');
+		return (0);
+}
