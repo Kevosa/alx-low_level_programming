acf681cf672ef06bb430f672f2720b173627d45a
Kevosa <yegokelvin00@gmail.com>
2023-06-20 12:21:42 -0700

    task14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 
-/**
- * main - check the code.
- *
- * Return: Always 0.
- */
 int main(void)
 {
 	int fib1 = 1;
 	int fib2 = 2;
 	int fib;
-
-	printf("%d, %d", fib1, fib2);
-
-	for (int i = 3; i <= 50; i++)
-	{
-	int fib = fib1 + fib2;
+    
+    printf("%d, %d", fib1, fib2);
+    
+    int i;
+	for (i = 3; i <= 50; i++)
+    {
+	fib = fib1 + fib2;
 	printf(", %d", fib);
 	fib1 = fib2;
 	fib2 = fib;
@@ -25,4 +21,3 @@ int main(void)
 
 	return 0;
 }
-
