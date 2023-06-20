626565614d96eaa7cf2d2480804cd2f3eda2c3c5
Kevosa <yegokelvin00@gmail.com>
2023-06-20 12:34:00 -0700

    task16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    unsigned int fib1 = 1;
+    unsigned int fib2 = 2;
+    unsigned int fib_next = 0;
+    int count = 2; // Number of Fibonacci numbers generated, starting with 1 and 2
+
+    printf("%u, %u", fib1, fib2);
+
+    while (count < 98) {
+        fib_next = fib1 + fib2;
+        printf(", %u", fib_next);
+
+        fib1 = fib2;
+        fib2 = fib_next;
+        count++;
+    }
+
+    printf("\n");
+
+    return 0;
+}
+
