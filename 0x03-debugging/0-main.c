c56081d9d6f854b77a60ef0dc3166c6b72eeb13f
Kevosa <yegokelvin00@gmail.com>
2023-06-22 12:48:17 -0700

    task1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,19 +5,11 @@
 * Return: 0
 */
 
-void positive_or_negative(int i)
-{
-	if (n > 0)
+int main(void)
 	{
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
 	return (0);
 }
