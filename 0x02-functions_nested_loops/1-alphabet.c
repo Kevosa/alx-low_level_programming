6650075fa375c1d20fb15706e6a5941ea024e055
Kevosa <yegokelvin00@gmail.com>
2023-06-22 02:56:33 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * main - print alphabet in small case
+ * print_alphabet - in small case
  *
  * Return: 0
  */
 void print_alphabet(void)
 {
 	char letter;
-	for (letter = 'a' ;letter <= 'z' ;letter++)
+
+	for (letter = 'a' ; letter <= 'z' ; letter++)
 	{
 		_putchar(letter);
 	}
