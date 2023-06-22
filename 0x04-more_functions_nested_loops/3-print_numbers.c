e13752497bf8965b59afcb24a345c41d1f8ffb2e
Kevosa <yegokelvin00@gmail.com>
2023-06-22 11:29:01 -0700

    task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0-9
+ * return: void
+ */
+
+void print_numbers(void)
+{
+	int k
+
+		if (k ==0 ; k <=9 ; k++)
+		{
+			_putchar(k);
+		}
+	_putchar('\n')
+		return (0);
+}
