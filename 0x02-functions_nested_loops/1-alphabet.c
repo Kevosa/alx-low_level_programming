b7ee08e3db3c3840c9d44305a1e68cb2a2006101
Kevosa <yegokelvin00@gmail.com>
2023-06-22 02:45:36 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - print alphabet in small case
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char letter;
+	if (letter = a ;letter <= z ;letter++)
+	{
+		_putchar(letter);
+	}
+		_putchar('\n');
+	return(0);
+}
