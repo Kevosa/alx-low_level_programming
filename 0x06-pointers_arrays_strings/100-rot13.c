3aa4916641cd1f00c66cca0eaf8b8a2e4d043f21
Kevosa <yegokelvin00@gmail.com>
2023-06-29 17:17:06 +0000

    yes

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,30 +1,34 @@
 #include "main.h"
 
 /**
- *  * rot13 - Encodes a string using ROT13 cipher.
- *   * @str: The string to encode.
- *    *
- *     * Return: Pointer to the resulting encoded string.
+ * rot13 - Encodes a string using ROT13 cipher.
+ * @str: The string to encode.
+ *
+ * Return: Pointer to the resulting encoded string.
  */
 char *rot13(char *str)
 {
-	char *ptr = str;
-	int i;
+    char *ptr = str;
+    int i;
+    char *input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    char *output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	char *input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char *output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+    while (*ptr)
+    {
+        for (i = 0; input[i]; i++)
+        {
+            if (*ptr == input[i])
+            {
+                *ptr = output[i];
+                break;
+            }
+        }
+        if (input[i])
+            ptr++;
+        else
+            break;
+    }
 
-	while (*ptr)
-	{
-	for (i = 0; input[i]; i++)
-	}
-	if (*ptr == input[i])
-	{
-		*ptr = output[i];
-		break;
-	}
-	{
-		ptr++;
-	}
-	return (str);
+    return str;
 }
+
