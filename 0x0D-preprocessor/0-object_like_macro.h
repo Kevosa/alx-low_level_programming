a0e131222f3fbb685b6480b35983e31aa1117abb
Kevosa <yegokelvin00@gmail.com>
2023-07-17 21:05:41 +0000

    prepocessor

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifdef _OBJECT_LIKE_MACRO_H
-#define _OBJECT_LIKE_MACRO_H
+#ifdef _object_like_macro
+#define _object_like_macro
 
 $define SIZE 1024
 
-#endif/* _OBJECT_LIKE_MACRO_H*/
+#endif /*_object_like_macro_h*/
