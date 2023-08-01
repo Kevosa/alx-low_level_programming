250db31eed8297b557558e6ecc287bb392418492
kevosa <yegokelvin00@gmail.com>
2023-08-01 20:33:26 +0000

    hotfix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,10 +15,10 @@ size_t print_list(const list_t *h);
 	while (current != NULL)
 	{
 		if (current->str == NULL)
-			printf("[0] (nil)\n");
 		
-		else
 			printf("[%d] %s\n", current->len, current->str);
+		else
+			printf("[0] (nil)\n");
 		
 		count++;
 		
