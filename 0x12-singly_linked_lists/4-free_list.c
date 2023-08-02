3b1ff456ca1438bba57d9f68030e994dba517ae5
kevosa <yegokelvin00@gmail.com>
2023-08-02 12:37:21 +0000

    adding betty doc

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,12 +1,14 @@
 #include "lists.h"
-
-
+/**
+ * free_list - Frees a linked list.
+ * @head: Pointer to the head of the linked list.
+ */
 void free_list(list_t *head)
 {
 	list_t *current = head;
 	list_t *next_node;
 
-	while(current != NULL)
+	while (current != NULL)
 	{
 		next_node = current->next;
 		free(current->str);
