378b9e87d11c2ac98337d1110e4be7de9a51be82
kevosa <yegokelvin00@gmail.com>
2023-08-02 12:47:10 +0000

    hotfix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,7 +2,7 @@
 /**
  * before_main - prints a string before main function
  */
-void before_main(void)
+void before_main(void)__attribute__((constructor));
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
