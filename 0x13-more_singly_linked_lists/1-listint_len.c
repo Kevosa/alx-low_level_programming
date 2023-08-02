3fe6933a80c5f5f2ebb83e0da35b3d5fd0258f9a
kevosa <yegokelvin00@gmail.com>
2023-08-02 13:16:18 +0000

    count number of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * listint_len - Prints integer elements of a listint_t list.
+ * @h: Pointer to the head of the list.
+ * Return: Number of nodes in the list.
+ */
+size_t listint_len(const listint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
