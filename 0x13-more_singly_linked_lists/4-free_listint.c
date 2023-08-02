9c2a5c5d8473edabb6877fc53a3e0c5d2f6e6eaf
kevosa <yegokelvin00@gmail.com>
2023-08-02 14:02:17 +0000

    too free

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include"lists.h"
+/**
+ * free_listint - Frees a listint_t linked list.
+ * @head: Pointer to the head of the list.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *next_node;
+	listint_t *current = head;
+
+	while (current != NULL)
+	{
+		next_node = current->next;
+		free(current);
+		current = next_node;
+	}
+}
+
