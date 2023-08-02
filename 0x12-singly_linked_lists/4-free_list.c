394215e4edd0414dbd7f6ac4b83cb1e2a623cca2
kevosa <yegokelvin00@gmail.com>
2023-08-02 12:27:37 +0000

    to free list_t

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+
+void free_list(lists_t *head)
+{
+	list_t *current = head;
+	list_t *next_node;
+
+	while(current != NULL)
+	{
+		next_node = current->next;
+		free(current->str);
+		free(current);
+		current = next_node;
+	}
+}
+
