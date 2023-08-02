a39d78d63d3f063608ab4dec9f1dbd1f113afc57
kevosa <yegokelvin00@gmail.com>
2023-08-02 14:06:23 +0000

    fix

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,7 +3,7 @@
  * free_listint2 - Frees a listint_t linked list.
  * @head: Pointer to the head of the list.
  */
-void free_listint2(listint_t *head)
+void free_listint2(listint_t **head)
 {
 	listint_t *next_node;
 	listint_t *current = head;
