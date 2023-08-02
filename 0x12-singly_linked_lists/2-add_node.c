5f6e0d43c6c8b7778e2962c381a3b70eacbd8611
kevosa <yegokelvin00@gmail.com>
2023-08-02 11:55:51 +0000

    fixing errors

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,26 +9,27 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	int length = 0;
+	char *duplicate;
 	list_t *new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 		return (NULL);
-	
-	char *duplicate = strdup(str);
+
+	duplicate = strdup(str);
 	if (duplicate == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-	while(str[length] != '\0')
+	while (str[length] != '\0')
 		length++;
 
 	new_node->next = *head;
 	new_node->str = duplicate;
 	new_node->len = length;
-	
-	*head=new_node;
-	
+
+	*head = new_node;
+
 	return (new_node);
 }
 
